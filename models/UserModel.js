60ed2369be5f36ea773de0fb324054434aff7938
jianhao <1498158052@qq.com>
2020-02-20 15:35:58 +0800

    '服务器

---
+++ models/UserModel.js
@@ -0,0 +1,25 @@
+/*
+能操作users集合数据的Model
+ */
+// 1.引入mongoose
+const mongoose = require('mongoose');
+
+// 2.字义Schema(描述文档结构)
+const userSchema = new mongoose.Schema({
+  // 用户名
+  name: { type: String },
+  // 密码
+  pwd: { type: String },
+  // 类型
+  phone: { type: String },
+  // 定义用户拥有的 信息
+  list: { type: String },
+  // 头像
+  avatar: { type: String }
+});
+
+// 3. 定义Model(与集合对应, 可以操作集合)
+const UserModel = mongoose.model('users', userSchema);
+
+// 4. 向外暴露Model
+module.exports = UserModel;
